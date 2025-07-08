5b44b8c5f488cbf63dde4015887f3dc677734b26
adarshvishwakarmaa <adarshvishwakarma2004@gmail.com>
2025-07-08 20:52:47 +0530

    Add project files.

---
+++ EMS MANAGMEMENT/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EMS_MANAGMEMENT.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS_MANAGMEMENT.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
